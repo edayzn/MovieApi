864a018c625ee808e6eb41538a5475049320fd8d
edayzn <eda.yzn5@gmail.com>
2025-06-02 00:28:38 +0300

    MediatR DI Registiration Uygulanması

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/TagHandler/GetTagByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Result.TagResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandler
+{
+    public class GetTagByIdQueryHandler : IRequestHandler<GetTagByIdQuery, GetTagByIdQueryResult>
+    {
+        private readonly MovieContext _context;
+
+        public GetTagByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetTagByIdQueryResult> Handle(GetTagByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Tags.FindAsync(request.TagId);
+            return new GetTagByIdQueryResult
+            {
+                TagId = values.TagId,
+                Title = values.Title
+            };
+        }
+    }
+}
--- Presentation/MovieApi.WebApi/Program.cs
+++ Presentation/MovieApi.WebApi/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.OpenApi.Models;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+using MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandler;
 using MovieApi.Persistence.Context;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +23,8 @@ builder.Services.AddScoped<CreateMovieCommandHandler>();
 builder.Services.AddScoped<RemoveMovieCommandHandler>();
 builder.Services.AddScoped<UpdateMovieCommandHandler>();
 
-
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTagQueryHandler).Assembly));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
